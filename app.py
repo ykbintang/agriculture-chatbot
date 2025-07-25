aa692ae01364a838a5bcb50cd94f3f1e33543e8b
Bintang <ykbintang07@gmail.com>
2025-07-26 00:13:01 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ def chat():
 if __name__ == "__main__":
     os.makedirs("uploads", exist_ok=True)
     os.makedirs("static/audio", exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
